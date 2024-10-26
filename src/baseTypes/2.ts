87779be63b2ebe7f3b1affb2b16452e78d32407a
Iryna Alchynska <iren0988@gmail.com>
2024-10-26 18:35:23 +0200

    add: types to tasks

--- src/baseTypes/2.ts
+++ src/baseTypes/2.ts
@@ -1,5 +1,10 @@
-let anything = -20;
+let anything:any = -20;
 anything = 'Text';
 anything = {};
 
 export {};
+
+
+// JavaScript змінна може зберігати значення будь-якого типу:
+
+// Який тип ви надаєте змінній anything в TypeScript, щоб зберегти її гнучкість?
